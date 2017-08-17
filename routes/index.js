0c5e669f4888227a1fa30d8caa24cbf5ab49e812
Bethany Priode <bpriode@gmail.com>
2017-08-17 09:06:52 -0400

    can add to completed, but not remove

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+
+
+const todos = ["Wash the Car", "Buy Bread"];
+
+const completed =["Make Dinner"];
+
+router.get("/", function (req, res) {
+  res.render('list', {todos: todos , completed: completed});
+});
+
+router.post("/list", function (req, res) {
+  todos.push(req.body.task);
+  // console.log(todos);
+  res.redirect('/');
+});
+
+
+router.post("/completed", function (req, res) {
+    completed.push(req.body.button);
+    res.redirect('/');
+});
+
+// completed = completed.filter(function(id){
+//   return completed.id !== id;
+// })
+
+
+
+
+
+
+module.exports = router;
